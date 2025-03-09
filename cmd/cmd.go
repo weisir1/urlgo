520e4f217a3e18cfb51d85e26f5d45fd98ff3d43
Administrator <1050966321@qq.com>
2025-03-09 10:48:42 +0800

    超时去除

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -26,7 +26,7 @@ var (
 	//nO string
 	X  string
 	T  = 50
-	TI = 50
+	TI = 5
 	MA = 99999
 	Z  int
 )
@@ -43,11 +43,9 @@ func init() {
 	flag.IntVar(&M, "m", 1, "set mode\n抓取模式 \n   1 normal\n     正常请求（默认,爬取的path路径不进行请求 \n   2 thorough\n     全面抓取（默认,对爬取的path路径进行请求,速度较慢） \n  ")
 	flag.IntVar(&MA, "max", 99999, "set maximum\n最大抓取链接数")
 	flag.StringVar(&O, "o", ".", "set outFile\n结果导出到xlsx文件,需指定导出文件目录及xlsx后缀,否则生成到当前目录下以时间命名")
-	//flag.StringVar(&O, "o", ".", "废弃")
-	//flag.StringVar(&O, "no", "", "不进行输出,打印到控制台")
 	flag.StringVar(&S, "s", "", "set Status\n显示指定状态码,all为显示全部（多个状态码用,隔开）")
 	flag.IntVar(&T, "t", 50, "set Thread\n设置线程数（默认50）")
-	flag.IntVar(&TI, "time", 5, "set Timeout\n设置超时时间（默认5,单位秒）")
+	//flag.IntVar(&TI, "time", 5, "set Timeout\n设置超时时间（默认5,单位秒）")
 	flag.StringVar(&U, "u", "", "set Url\n目标URL")
 	flag.StringVar(&X, "x", "", "set Proxy\n设置代理,格式: http://username:password@127.0.0.1:8809")
 	//flag.IntVar(&Z, "z", 0, "set Fuzz\n对404链接进行fuzz(只对主域名下的链接生效,需要与 -s 一起使用） \n   1 decreasing\n     目录递减fuzz \n   2 2combination\n     2级目录组合fuzz（适合少量链接使用） \n   3 3combination\n     3级目录组合fuzz（适合少量链接使用） ")
@@ -57,7 +55,8 @@ func init() {
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, `Usage: URLGo [-a user-agent] [-b baseurl] [-c cookie][-f urlFile] [-h help]  [-i configFile]  [-m mode] [-max maximum] [-o outFile]  [-s Status] [-t thread] [-time timeout] [-u url] [-x proxy] 
+	//[-time timeout]
+	fmt.Fprintf(os.Stderr, `Usage: URLGo [-a user-agent] [-b baseurl] [-c cookie][-f urlFile] [-h help]  [-i configFile]  [-m mode] [-max maximum] [-o outFile]  [-s Status] [-t thread] [-u url] [-x proxy] 
 
 Options:
 `)
